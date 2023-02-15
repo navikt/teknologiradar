38e9574dce2559fa977e773858285703a96189eb
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2023-02-15 11:56:27 +0100

    Extract ActivityLocation to component

--- src/pages/activities.tsx
+++ src/pages/activities.tsx
@@ -3,6 +3,7 @@ import { Detail, Heading, Ingress, Panel } from "@navikt/ds-react";
 import Link from "next/link";
 import Image from "next/image";
 import { getExampleData, LearningActivity } from "@/lib/activities";
+import { ActivityLocation } from "@/components/ActivityLocation";
 
 export async function getServerSideProps() {
   const activities: LearningActivity[] = await getExampleData();
@@ -23,65 +24,6 @@ const ActivitiesPage: NextPage<{ activities: LearningActivity[] }> = ({
 
 const TimeLabel = ({ time }: { time: string }) => {
   return <span className={"time-label"}>{time}</span>;
-};
-
-const parseLocation = (
-  location: string
-): { label: string; link?: string; icon?: string; iconAlt?: string } => {
-  if (/\.zoom\.us/.test(location)) {
-    return {
-      link: location,
-      label: "Zoom-møte",
-      icon: "/img/zoom.png",
-      iconAlt: "Zoom",
-    };
-  }
-  if (/teams\.microsoft\.com/.test(location)) {
-    return {
-      link: location,
-      label: "Teams-møte",
-      icon: "/img/teams.png",
-      iconAlt: "Teams",
-    };
-  }
-  if (/^#/.test(location)) {
-    const slackChannelName = location.substring(1).trim();
-    const slackUrl = `https://nav-it.slack.com/archives/${slackChannelName}`;
-    return {
-      link: slackUrl,
-      label: slackChannelName,
-      icon: "/img/slack.png",
-      iconAlt: "Slack-kanal",
-    };
-  }
-  return {
-    label: location,
-    icon: "/img/fya1.jpg",
-    iconAlt: "Fyrstikkalléen 1",
-  };
-};
-
-const ActivityLocation = ({ location }: { location: string }) => {
-  const iconSize = 20;
-  const loc = parseLocation(location);
-
-  return (
-    <span className={"activity--location"}>
-      {loc.icon && (
-        <Image
-          src={loc.icon}
-          alt={loc.iconAlt!}
-          width={iconSize}
-          height={iconSize}
-        />
-      )}
-      {loc.link ? (
-        <Link href={loc.link}>{loc.label}</Link>
-      ) : (
-        <span>{loc.label}</span>
-      )}
-    </span>
-  );
 };
 
 const ActivityEntry = ({ activity }: { activity: LearningActivity }) => {

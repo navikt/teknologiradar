5004d203e17d24dd84d881c0c035b39b9c3ac32f
EIlif Johansen <eilifjohansen@outlook.com>
2023-12-01 01:48:03 +0100

    La til backend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -70,7 +70,13 @@ const ActivitiesPage: NextPage<{
   const dates = Object.keys(groupedByDate);
   dates.sort((a, b) => (a === b ? 0 : a > b ? -1 : 1));
 
-  const options = ["Design", "Frontend", "Data science", "Data engineering"];
+  const options = [
+    "Design",
+    "Frontend",
+    "Backend",
+    "Data science",
+    "Data engineering",
+  ];
   const [selected, setSelected] = useState([]);
 
   const options2 = ["Bruk", "Vurder", "Avstå"];

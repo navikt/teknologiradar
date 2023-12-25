f1c9684330f638376f043eb0f58528af4be13818
EIlif Johansen <eilifjohansen@outlook.com>
2023-12-25 11:55:37 +0100

    Lagde et eget header komponent

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import { Header } from "@navikt/ds-react-internal";
+import NextLink from "next/link";
+
+function MyHeader() {
+  return (
+    <Header>
+      <Header.Title as="h1">
+        <NextLink
+          id="logo"
+          href="."
+          className="inline-flex items-center gap-3 -ml-1 hover:underline underline-offset-4 color-blue"
+        >
+          <svg
+            className="mt-px w-7 h-7 sm:w-9 sm:h-9"
+            xmlns="http://www.w3.org/2000/svg"
+            width="36"
+            height="36"
+            fill="none"
+            viewBox="0 0 36 36"
+            aria-hidden="true"
+            focusable="false"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeWidth="1.75"
+          >
+            <path d="M2 18A16 16 0 1 0 29.7 7m-23-.3a16 16 0 0 1 19.8-2.2" />
+            <path d="M12.3 12.3a8 8 0 1 1 2.3 13M10 18a8 8 0 0 0 1.5 4.6M18 18 3 3" />
+            <circle cx="28" cy="5.8" r="2" />
+            <circle cx="13" cy="24" r="2" />
+          </svg>
+          <span className="font-semibold">Teknologiradar βeta</span>
+        </NextLink>
+      </Header.Title>
+    </Header>
+  );
+}
+
+export default MyHeader;

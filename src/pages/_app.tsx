db5f5b22b80d6779f01f0343a9a8ebff464996eb
EIlif Johansen <eilifjohansen@outlook.com>
2023-12-25 23:13:52 +0100

    La til scroll til toppen knapp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "@navikt/ds-css-internal";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import ScrollToTop from "@/components/ScrollToTop";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </main>
       <Footer />
+      <ScrollToTop />
     </div>
   );
 }

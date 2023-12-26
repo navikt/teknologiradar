9e23ba89ef7417441f6ded628fb366262ec08dea
EIlif Johansen <eilifjohansen@outlook.com>
2023-12-26 01:15:25 +0100

    Fikset bug knyttet til footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <main className="layout">
         <Component {...pageProps} />
       </main>
-      <Footer className="layout" />
+      <Footer />
       <ScrollToTop />
     </div>
   );

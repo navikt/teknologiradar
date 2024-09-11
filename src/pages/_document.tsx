852d82c22daca7fbf6800fae48ea2dcd6eed38eb
EIlif Johansen <eilifjohansen@outlook.com>
2024-09-11 16:18:38 +0200

    La til sporing på CDN

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
                 <script
                     defer
                     data-domains="teknologiradar.ansatt.nav.no"
-                    src="https://umami.nav.no/script.js"
+                    src="https://cdn.nav.no/team-researchops/sporing/sporing.js" data-host-url="https://umami.nav.no"
                     data-website-id="f8157d1d-b9ff-4208-9281-882e4594c126"
                 ></script>
             </Head>

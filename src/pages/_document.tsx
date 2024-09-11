4675faf58af917e2d76f25aee0c991a876de0023
EIlif Johansen <eilifjohansen@outlook.com>
2024-09-11 13:38:31 +0200

    Tester sporingskode Umami

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,20 @@
 import { Html, Head, Main, NextScript } from "next/document";
+
 export default function Document() {
-  return (
-    <Html lang="no">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="no">
+            <Head>
+                <script
+                    defer
+                    data-domains="https://teknologiradar.ansatt.nav.no/"
+                    src="https://umami.nav.no/script.js"
+                    data-website-id="f8157d1d-b9ff-4208-9281-882e4594c126"
+                ></script>
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    );
 }

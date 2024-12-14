8bf41082247bf4aa1d805d848ea43a847f93ea7b
Espen Holtebu <espen.holtebu@nav.no>
2024-12-14 07:09:39 +0100

    Correct format on trello lists

--- scripts/get-trello-lists.mjs
+++ scripts/get-trello-lists.mjs
@@ -9,11 +9,13 @@ const trelloApiToken = process.env["TRELLO_API_TOKEN"];
 const url = `https://api.trello.com/1/boards/${trelloBoardId}/lists`;
 const authorization = `OAuth oauth_consumer_key=\"${trelloApiKey}\", oauth_token=\"${trelloApiToken}\"`;
 
+
 (async () => {
   try {
     const resp = await fetch(url, { headers: { authorization } });
     const data = await resp.json();
-    console.log(data.map((list) => ({ id: list.id, name: list.name })));
+    console.log(data.reduce((accumulatorObject, currentTrelloList) => (
+      { ...accumulatorObject, [currentTrelloList.id]: currentTrelloList.name}), {}));
   } catch (e) {
     console.error("Error fetching Trello cards", e);
   }
